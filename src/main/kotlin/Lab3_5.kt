6daa5a0232568ff787737067c8c0e4c76a5053bd
Nick <bbamrin@gmail.com>
2021-05-10 22:29:29 +0300

    made lab 3.5

--- src/main/kotlin/Lab3_5.kt
+++ src/main/kotlin/Lab3_5.kt
@@ -1,2 +1,68 @@
+import com.sun.marlin.DPathConsumer2D
+import kotlin.math.pow
+
 class Lab3_5 {
+//    fun squares
+
+    private val interpolator = Lab3_1()
+
+    fun rectangleMethod( a: Double, b: Double, h: Double, f: (Double) -> Double): Double {
+        var i = a + h
+        var result = 0.0
+        while (i <= b) {
+            val x = i - h/2
+            result += interpolator.lagrangeInterpolation(x, listOf(Pair(x, f(x))))
+            i += h
+        }
+        return result * h
+    }
+
+    fun trapezoidalMethod( a: Double, b: Double, h: Double, f: (Double) -> Double): Double {
+        var i = a + h
+        var result = 0.0
+        while (i <= b) {
+            val nodes = listOf(
+                Pair(i - h, f(i - h)),
+                Pair(i, f(i))
+            )
+            val f1 = interpolator.lagrangeInterpolation(i - h, nodes)
+            val f2 = interpolator.lagrangeInterpolation(i, nodes)
+            result += (f1 + f2)
+            i += h
+        }
+        return result * h/2
+    }
+
+    fun simpsonMethod( a: Double, b: Double, h: Double, f: (Double) -> Double): Double {
+        var i = a + 2*h
+        var result = 0.0
+        while (i <= b) {
+            val nodes = listOf(
+                Pair(i - 2*h, f(i - 2*h)),
+                Pair(i - h, f(i - h)),
+                Pair(i, f(i))
+            )
+            val f1 = interpolator.lagrangeInterpolation(i - 2*h, nodes)
+            val f2 = 4 * interpolator.lagrangeInterpolation(i - h, nodes)
+            val f3 = interpolator.lagrangeInterpolation(i, nodes)
+            result += (f1 + f2 + f3)
+            i += 2*h
+        }
+        return result * h/3
+    }
+
+    fun rungeRombergMethod(
+        f: (Double, Double, Double, (Double) -> Double) -> Double,
+        g: (Double) -> Double,
+        a: Double,
+        b: Double,
+        h1: Double,
+        h2: Double,
+        p: Int
+    ): Double {
+        val f1 = f(a, b, h1, g)
+        val f2 = f(a, b, h2, g)
+        return f1 + (f1 - f2)/((h2/h1).pow(p) - 1)
+    }
 }
+
--- src/main/kotlin/MainLab3_5.kt
+++ src/main/kotlin/MainLab3_5.kt
@@ -1,2 +1,31 @@
+import kotlin.math.pow
+
 class MainLab3_5 {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val lab = Lab3_5()
+            val f: (Double) -> Double = {
+                (3*it + 4)/(2*it + 7)
+            }
+            val f1: (Double) -> Double = {
+               it/(3*it + 4).pow(2)
+            }
+            val x0 = -1.0
+            val x1 = 1.0
+            val h1 = 0.5
+            val h2 = 0.25
+            println("rectangle h = $h1: ${lab.rectangleMethod(x0, x1, h1, f1)}")
+            println("trapezoidal h = $h1: ${ lab.trapezoidalMethod(x0, x1, h1, f1) }")
+            println("simpson h = $h1: ${ lab.simpsonMethod(x0, x1, h1, f1) }")
+            println("rectangle h = $h2: ${lab.rectangleMethod(x0, x1, h2, f1)}")
+            println("trapezoidal h = $h2: ${ lab.trapezoidalMethod(x0, x1, h2, f1) }")
+            println("Simpson h = $h2: ${ lab.simpsonMethod(x0, x1, h2, f1) }")
+            println("Runge-Romberg error: ${lab.rungeRombergMethod(lab::rectangleMethod, f1, x0, x1, h1, h2, 1)}")
+            println("Runge-Romberg error: ${lab.rungeRombergMethod(lab::trapezoidalMethod, f1, x0, x1, h1, h2, 2)}")
+            println("Runge-Romberg error: ${lab.rungeRombergMethod(lab::simpsonMethod, f1, x0, x1, h1, h2, 4)}")
+
+        }
+
+    }
 }
